90d6c3e77cc3422dec3fa54d884e7d48aba42e78
JI,XIAOYONG <jixiaoyong1995@gmail.com>
2024-03-05 16:19:31 +0800

    feat: 更新弹窗样式

--- lib/component/settings_dialog.dart
+++ lib/component/settings_dialog.dart
@@ -31,15 +31,24 @@ class SettingsDialog extends StatefulWidget {
 }
 
 class _SettingsDialogState extends State<SettingsDialog> {
+  late TextEditingController _searchIconController;
+
+  @override
+  void initState() {
+    super.initState();
+    _searchIconController =
+        TextEditingController(text: widget.searchIcon ?? "");
+  }
+
   @override
   Widget build(BuildContext context) {
     var fontSize = widget.fontSize;
-    var searchIcon = widget.searchIcon;
     var isDarkTheme = widget.isDarkTheme;
 
-    var textStyle = TextStyle(
-      color: Colors.grey[100],
-    );
+    var textStyle = TextStyle(color: Colors.grey[100]);
+    var screenWidth = MediaQuery.of(context).size.width;
+    var isSmallScreen = screenWidth <= 500;
+    var dialogWidth = (isSmallScreen ? 0.95 : 0.5) * screenWidth;
 
     return AlertDialog(
       title: Text(
@@ -49,79 +58,84 @@ class _SettingsDialogState extends State<SettingsDialog> {
       backgroundColor: Colors.black.withOpacity(0.6),
       contentTextStyle: textStyle,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      scrollable: true,
       content: StatefulBuilder(
         builder: (context, innerState) {
-          return Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SettingsItem(
-                title: "文字大小:$fontSize",
-                child: Slider(
-                  value: fontSize,
-                  onChanged: (value) {
-                    widget.updateFontSize(value);
-                    fontSize = value;
-                    innerState(() {});
-                  },
-                  divisions: 10,
-                  min: 10,
-                  max: 20,
-                  label: "$fontSize",
+          return SizedBox(
+            width: dialogWidth,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SettingsItem(
+                  title: "文字大小:$fontSize",
+                  child: Slider(
+                    value: fontSize,
+                    onChanged: (value) {
+                      widget.updateFontSize(value);
+                      fontSize = value;
+                      innerState(() {});
+                    },
+                    divisions: 10,
+                    min: 10,
+                    max: 20,
+                    label: "$fontSize",
+                  ),
                 ),
-              ),
-              SettingsItem(
-                title: "设置头像",
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: "请输入头像地址",
-                          hintStyle: textStyle,
+                SettingsItem(
+                  title: "设置头像",
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "请输入头像地址",
+                            hintStyle: textStyle,
+                          ),
+                          style: textStyle,
+                          maxLines: 2,
+                          controller: _searchIconController,
                         ),
-                        onChanged: (value) {
-                          searchIcon = value;
-                          innerState(() {});
-                          Logger.d("输入头像地址 $searchIcon");
-                        },
-                        maxLines: 1,
                       ),
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.check),
-                      iconSize: 20,
-                      onPressed: () {
-                        innerState(() {});
-                        if (searchIcon == null || searchIcon?.isEmpty == true) {
-                          return;
-                        }
-                        Logger.d("searchIcon输入头像地址 $searchIcon");
-                        widget.updateSearchIcon(searchIcon!);
-                        Navigator.of(context).pop();
-                      },
-                    )
-                  ],
-                ),
-              ),
-              SettingsItem(
-                title: "修改主题",
-                child: Row(
-                  children: [
-                    Text(
-                      isDarkTheme ? "夜间模式" : "白天模式",
-                    ),
-                    Switch(
-                        value: isDarkTheme,
-                        onChanged: (bool newValue) {
-                          isDarkTheme = newValue;
-                          widget.updateDarkModel(newValue);
+                      IconButton(
+                        icon: Icon(
+                          Icons.check_circle,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        iconSize: 25,
+                        onPressed: () {
+                          var searchIcon = _searchIconController.text;
+                          if (true == searchIcon.isEmpty) {
+                            return;
+                          }
+                          Logger.d("searchIcon输入头像地址 $searchIcon");
+                          widget.updateSearchIcon(searchIcon);
                           innerState(() {});
-                        }),
-                  ],
+                        },
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                SettingsItem(
+                  title: "修改主题",
+                  child: Row(
+                    children: [
+                      Text(
+                        isDarkTheme ? "夜间模式" : "白天模式",
+                      ),
+                      const Spacer(),
+                      Switch(
+                          value: isDarkTheme,
+                          onChanged: (bool newValue) {
+                            isDarkTheme = newValue;
+                            widget.updateDarkModel(newValue);
+                            innerState(() {});
+                          }),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           );
         },
       ),
