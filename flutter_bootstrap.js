6d733fa841ee713be25bae8f9211f115e67acc9d
jixiaoyong <jixiaoyong@users.noreply.github.com>
2025-10-31 09:36:44 +0000

    deploy: 8d6c7144cd5a95db5a04c8bd38509219a33f0b86

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1637209332"
+    serviceWorkerVersion: "3068526696"
   }
 });
